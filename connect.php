912d4258a3cda4cee89ed6b3df064021065b5814
Brandon Behbahani <brandonbehbahani@gmail.com>
2018-02-16 16:09:00 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$db_name = "<enter_DBname_here";
+$db_name = "<enter_DBname_here>";
 $mysql_username = "<enter_username_here>";
 $mysql_password = "<Enter_Password_here>";
 $server_name = "localhost";
