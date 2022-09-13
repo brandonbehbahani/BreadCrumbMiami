1591ef7f1d75f0fcf27b2104dd83fb9df56faf9c
Brandon Behbahani <brandonbehbahani@gmail.com>
2022-09-13 15:51:30 -0400

    Adding web version

---
+++ android/connect.php
@@ -0,0 +1,8 @@
+<?php
+$db_name = "<enter_DBname_here>";
+$mysql_username = "<enter_username_here>";
+$mysql_password = "<Enter_Password_here>";
+$server_name = "localhost";
+$conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
+
+?>
